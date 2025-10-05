379ad566e78bd4ef478ad7025a3023c9c7c2524b
nambatu <julianlangschwert@gmail.com>
2025-10-05 19:02:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ const client = new Client({
     puppeteer: {
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: '/usr/bin/chromium-browser'
+        executablePath: '/usr/bin/chromium'
     }
 });
 
@@ -282,3 +282,4 @@ process.on('SIGINT', async () => {
     process.exit(0);
 
 });
+
